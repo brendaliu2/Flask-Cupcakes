fffa359afed5fcc9653cbdc4afbf204b77eaf7c4
Andrew Jensen <andrewkjensen@gmail.com>
2022-08-08 17:25:26 -0700

    make fully functional

--- static/app.js
+++ static/app.js
@@ -3,16 +3,17 @@
 
 // TODO: OOP make it a class
 
-const $list = $('ul');
+const $LIST = $('ul');
 
-async function get_cupcakes_json () {
+async function getCupcakesJson () {
   let response = await axios.get('api/cupcakes');
   console.info("get_cupcakes_json");
   return response.data.cupcakes;
 }
 
-async function append_to_cupcake_list(){
-  const cupcakes = await get_cupcakes_json();
+async function appendToCupcakeList(){
+  $LIST.empty();
+  const cupcakes = await getCupcakesJson();
   for(let cupcake of cupcakes){
     const flavor = cupcake.flavor;
     const size = cupcake.size;
@@ -24,12 +25,48 @@ async function append_to_cupcake_list(){
     const $image = $("<img>");
     $image.attr('src', image);
     $cupcake.text(cupcakeString);
-    $image.attr('width', "200px");
+    $image.attr('width', "50px");
     $image.appendTo($cupcake);
-    $cupcake.appendTo($list);
+    $cupcake.appendTo($LIST);
 
   }
 }
 
-append_to_cupcake_list()
+function getFormData () {
+  return {
+    'flavor': $('#flavor').val(),
+    'size': $('#size').val(),
+    'rating': $('#rating').val(),
+    'image': $('#image').val(),
+  }
+}
+
+function clearForm() {
+  $('#flavor').val('');
+  $('#size').val('');
+  $('#rating').val('');
+  $('#image').val('');
+}
+
+async function addCupcake () {
+  const newCupcake = getFormData();
+  clearForm();
+  const resp = await axios.post('api/cupcakes', newCupcake);
+  console.log(resp)
+}
+
+
+function submitHandler(e) {
+  e.preventDefault(); 
+  addCupcake();
+  appendToCupcakeList();
+}
+
+
+$('form').on('submit',submitHandler);
+
+
+
+appendToCupcakeList();
+
 
